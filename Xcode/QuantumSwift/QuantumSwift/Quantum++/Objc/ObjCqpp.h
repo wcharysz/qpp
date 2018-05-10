455a092524792244666cfec6afc5c892c81a3c98
Wojciech Charysz <>
2018-05-10 22:04:09 +0200

    Compiling qpp with Eigen 3

---
+++ Xcode/QuantumSwift/QuantumSwift/Quantum++/Objc/ObjCqpp.h
@@ -0,0 +1,13 @@
+//
+//  ObjCqpp.h
+//  QuantumSwift
+//
+//  Created by Wojciech Charysz on 10.05.2018.
+//  Copyright © 2018 Wojciech Charysz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjCqpp : NSObject
+
+@end
---
+++ Xcode/QuantumSwift/QuantumSwift/Quantum++/Objc/ObjCqpp.mm
@@ -0,0 +1,14 @@
+//
+//  ObjCqpp.m
+//  QuantumSwift
+//
+//  Created by Wojciech Charysz on 10.05.2018.
+//  Copyright © 2018 Wojciech Charysz. All rights reserved.
+//
+
+#import "ObjCqpp.h"
+#import <qpp.h>
+
+@implementation ObjCqpp
+
+@end
