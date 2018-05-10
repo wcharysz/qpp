3d2603a700baa8f9a3e4826de3707fbe8f1c1a9b
Wojciech Charysz <>
2018-05-10 17:57:16 +0200

    Base Xcode project

---
+++ Xcode/QuantumSwift/QuantumSwift/QuantumSwift.h
@@ -0,0 +1,19 @@
+//
+//  QuantumSwift.h
+//  QuantumSwift
+//
+//  Created by Wojciech Charysz on 10.05.2018.
+//  Copyright © 2018 Wojciech Charysz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for QuantumSwift.
+FOUNDATION_EXPORT double QuantumSwiftVersionNumber;
+
+//! Project version string for QuantumSwift.
+FOUNDATION_EXPORT const unsigned char QuantumSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuantumSwift/PublicHeader.h>
+
+
