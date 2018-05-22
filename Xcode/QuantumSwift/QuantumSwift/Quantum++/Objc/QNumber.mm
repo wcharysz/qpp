9d6ee33631537c34450fbf328ae76e476dde9615
Wojciech Charysz <>
2018-05-22 20:45:11 +0200

    changing the loop

--- Xcode/QuantumSwift/QuantumSwift/Quantum++/Objc/QNumber.mm
+++ Xcode/QuantumSwift/QuantumSwift/Quantum++/Objc/QNumber.mm
@@ -30,25 +30,31 @@
 }
 
 - (void)binaryFromInteger {
-    using namespace std;
+    using namespace qpp;
     
     //Count bits:
-    int leadingZeros = __builtin_clzl(self.integerValue);
     int numberOfBits = sizeof(NSInteger) * CHAR_BIT;
-    numberOfBits -= leadingZeros;
+    numberOfBits -= __builtin_clzl(self.integerValue);;
     int binaryDigit = 1;
     
     do {
-        // Int as Binary bit: 1 or 0
+        // Int as Binary bit
+        if (self.integerValue & binaryDigit) {
+            //bit: 1
+
+        } else {
+            //bit: 0
+        }
+        
         short binaryBit = self.integerValue & binaryDigit ? 1 : 0;
         NSLog(@"binaryBit %d", binaryBit);
+        
         --numberOfBits;
         //Move to the next bit
         binaryDigit <<= 1;
         
     } while (numberOfBits);
     
-    using namespace qpp;
     ket G0 = 000_ket;
     ket G1 = 000_ket;
 }
